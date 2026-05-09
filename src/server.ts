0097cbdb0b286aefae4b7dc3a83c385dcd882d8c
Siiwowowow <siaa32290@gmail.com>
2026-05-09 13:57:21 +0600

    first commit

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import app from "./app";
+
+
+const bootstrap = async () => {
+    try {
+        app.listen (5000, () => {
+            console.log(`Server is running on http://localhost:5000`);
+        });
+    }catch(error){
+        console.error("Error starting the server:", error);
+    }
+}
+bootstrap();
+   
